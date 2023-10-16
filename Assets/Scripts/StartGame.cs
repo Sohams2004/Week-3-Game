8b35891dcbbe5bd81b775e42107b27014778d8fe
Jamal-khj <114579024+Jamal-khj@users.noreply.github.com>
2023-10-16 14:09:21 +0400

    Start Menu Done

---
+++ Assets/Scripts/StartGame.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class StartGame : MonoBehaviour
+{
+    public void LoadGame()
+    {
+        Time.timeScale = 1;
+        SceneManager.LoadScene("Tutorial 1");
+    }
+}
